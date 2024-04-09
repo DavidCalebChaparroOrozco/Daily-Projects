468a580d4ca5b0c0dd7fba162018f1528dc1b32a
David Caleb Chaparro Orozco <davidcaleb1998@hotmail.com>
2024-04-08 23:36:35 -0500

    Day 27

--- MLwithFastAPI/train_model.py
+++ MLwithFastAPI/train_model.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pickle
 from sklearn.datasets import fetch_openml
 from sklearn.ensemble import RandomForestClassifier
@@ -13,4 +14,21 @@ clf.fit(X_train, y_train)
 print(clf.score(X_test, y_test))
 
 with open('mnist_model.pkl', 'wb') as file:
+=======
+import pickle
+from sklearn.datasets import fetch_openml
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = RandomForestClassifier(n_jobs=-1)
+clf.fit(X_train, y_train)
+
+print(clf.score(X_test, y_test))
+
+with open('mnist_model.pkl', 'wb') as file:
+>>>>>>> df0de19 (Day 27)
     pickle.dump(clf, file)
