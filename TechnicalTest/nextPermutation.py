a7f82a15ded1548c84bafad971520ff5d7b12c55
David Caleb Chaparro Orozco <davidcaleb1998@hotmail.com>
2024-06-16 20:26:08 -0500

    Day 94

--- TechnicalTest/nextPermutation.py
+++ TechnicalTest/nextPermutation.py
@@ -1,3 +1,9 @@
+# Implement the next permutation, which rearranges numbers into the lexicographically next greater 
+# permutation of numbers. If such an arrangement is not possible, it must rearrange it as the 
+# lowest possible order (i.e., sorted in ascending order). 
+# The replacement must be in place and use only constant extra memory.
+
+# Importing necessary libraries
 from typing import List
 
 class Solution:
