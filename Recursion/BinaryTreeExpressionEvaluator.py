3e31dd546beb04ca537792264dd987d8397cdd98
David Caleb Chaparro Orozco <davidcaleb1998@hotmail.com>
2024-07-09 21:49:05 -0500

    Day 117

--- Recursion/BinaryTreeExpressionEvaluator.py
+++ Recursion/BinaryTreeExpressionEvaluator.py
@@ -48,6 +48,20 @@ def evaluate_tree(root):
     elif root.value == '^':
         return left_value ** right_value
 
+# Print the binary tree structure.
+def print_tree(root, level=0, prefix='Root: '):
+    """
+    Args:
+    root (TreeNode): The root node of the binary tree.
+    level (int): The current level of the node (default is 0).
+    prefix (str): The prefix to indicate the type of node (default is 'Root: ').
+    """
+    if root is not None:
+        print(' ' * (level * 4) + prefix + str(root.value))
+        if root.left is not None or root.right is not None:
+            print_tree(root.left, level + 1, 'Left: ')
+            print_tree(root.right, level + 1, 'Right: ')
+
 # Example usage
 
 # Create a binary tree for the expression: ((3 + 2) * (4 - 1)) ^ 2
@@ -63,6 +77,10 @@ root.left.left.right = TreeNode(2)
 
 root.left.right.left = TreeNode(4)
 root.left.right.right = TreeNode(1)
+
+# Print the binary tree structure
+print("Binary Tree Structure:")
+print_tree(root)
 
 # Evaluate the binary tree
 result = evaluate_tree(root)
