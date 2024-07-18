e590a26926d0a0a7a80766d424935bfbde026059
David Caleb Chaparro Orozco <davidcaleb1998@hotmail.com>
2024-07-17 23:06:33 -0500

    Day 125

--- TechnicalTest/multiply.py
+++ TechnicalTest/multiply.py
@@ -1,3 +1,8 @@
+# Given two non-negative integers num1 and num2 represented as strings, 
+# return the product of num1 and num2, also represented as a string.
+
+# Note: You must not use any built-in BigInteger library or convert 
+# the inputs to integer directly.
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         if num1 == "0" or num2 == "0":
