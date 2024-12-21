47d707414157f4c97dce1c47618bac0f101991ea
David Caleb Chaparro Orozco <davidcaleb1998@hotmail.com>
2024-12-20 21:34:08 -0500

    Day 279

---
+++ DataEngineer/Week_1/Day01/connetion.sql
@@ -0,0 +1,16 @@
+USE [master]
+GO
+CREATE LOGIN [etl] WITH PASSWORD = N'demopass', DEFAULT_DATABASE=AdventureWorks2019, CHECK_EXPIRATION = OFF, CHECK_POLICY=OFF
+GO
+USE [AdventureWorks2019]
+GO
+CREATE USER [etl] FOR LOGIN [etl]
+GO
+USE AdventureWorks2019
+GO
+ALTER ROLE db_datareader ADD MEMBER [etl]
+GO
+USE [master]
+GO
+GRANT CONNECT SQL TO [etl]
+GO
